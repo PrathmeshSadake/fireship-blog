26e4758d318aa166cdafc70ba761cd8d201b4082
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-10-07 12:35:04 +0530

    cleaned project

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
-  env: {
-    DB_LOCAL_URI: "mongodb://localhost:27017/hotellocato",
-  },
-};
--- redux/reducers/reducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { allRoomsReducers, roomDetailsReducers } from "./roomReducers";
-
-const reducers = combineReducers({
-  allRooms: allRoomsReducers,
-  room: roomDetailsReducers,
-});
-
-export default reducers;
